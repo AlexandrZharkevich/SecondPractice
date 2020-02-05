0b73a5e2ca76e38743566bedbadbd2c5900a8fa2
Aleksandr Zharkevich <alexandr.zharkevich@gmail.com>
2020-02-05 19:37:26 +0300

    Моря и острова

--- Practice2-2.py
+++ Practice2-2.py
@@ -1,1 +1,56 @@
 # Классы – Море, Континент, Государство, Остров, Суша;
+
+
+class Sea:
+    def __init__(self, title):
+        self.__title = title
+        self.__islands = []
+
+    def get_title(self):
+        return self.__title
+
+    def add_island(self, island):
+        self.__islands.append(island)
+
+    def show_islands(self):
+        for i in range(len(self.__islands)):
+            print(self.__islands[i])
+
+
+class Land:
+    def __init__(self):
+        self.__states = []
+
+    def add_state(self, state):
+        self.__states.append(state)
+
+    def show_states(self):
+        for i in range(len(self.__states)):
+            self.__states[i].show_title()
+
+
+class Island(Land):
+    def __init__(self, title):
+        Land.__init__(self)
+        self.__title = title
+
+    def show_title(self):
+        print(self.__title)
+
+    def show_states(self):
+        Land.show_states(self)
+
+
+class State:
+    def __init__(self, title):
+        self.__title = title
+
+    def show_title(self):
+        print(self.__title)
+
+
+belarus = State("Belarus")
+madagascar = Island("Madagascar")
+madagascar.add_state(belarus)
+madagascar.show_title()
+madagascar.show_states()
